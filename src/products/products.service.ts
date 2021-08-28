24012afd6181f5f0ec526d99cda39487bee88714
davi-OD <davidodie51@gmail.com>
2021-08-28 15:46:43 +0300

    Worked on get and post

---
+++ src/products/products.service.ts
@@ -0,0 +1,31 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { Product } from "./product.model";
+
+@Injectable()
+export class ProductsService {
+    private products: Product[] = [];
+
+    insertProduct(title: string, desc: string, price: number) {
+        const prodId = Math.random().toString();
+        const newProduct = new Product(prodId, title, desc, price)
+        this.products.push(newProduct);
+        return prodId;
+    }
+
+    getProducts() {
+        return [...this.products];
+    }
+
+    getSingleProduct(productId: string) {
+        const product = this.products.find((prod) => prod.id === productId);
+        if (!product) {
+            throw new NotFoundException('Cannot find product');
+        }
+        return { ...product };
+    }
+
+    updateProduct(productId: string, title: string, desc: string, price: number) {
+
+    }
+    
+}
